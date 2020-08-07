f2ae891759fbf368347f918785400a3a55447950
Ahmed E. Azzam <ahmede.azzam@gmail.com>
2020-08-07 21:46:22 +0300

    map v 0.2

--- client/src/components/mainMap/map.js
+++ client/src/components/mainMap/map.js
@@ -1,20 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from '../mainComp/navbar';
 import Filter from '../mainComp/filterComp';
-//import dotenv from 'dotenv';
+import * as allData from '../../data/data.json';
 
-// import { GoogleMap, withScriptjs, withGoogleMap } from 'react-google-maps';
-// function Maps() {
-//   return (
-//     <GoogleMap
-//       defaultZoom={10}
-//       defaultCenter={{ lat: 31.3547, lng: 34.3088 }}
-//     />
-//   );
-// }
-
-// const WrappedMap = withScriptjs(withGoogleMap(Maps));
-/*******/
 import {
   withScriptjs,
   withGoogleMap,
@@ -24,17 +12,16 @@ import {
 
 const MyMapComponent = withScriptjs(
   withGoogleMap((props) => (
-    <GoogleMap
-      defaultZoom={8}
-      defaultCenter={{ lat: 31.3547, lng: 34.3088 }}
-    ></GoogleMap>
+    <GoogleMap defaultZoom={12} defaultCenter={{ lat: 31.3547, lng: 34.3088 }}>
+      {allData.data.map((row) => (
+        <Marker position={{ lat: Number(row.lat), lng: Number(row.lng) }} />
+      ))}
+    </GoogleMap>
   ))
 );
 
 class Map extends React.Component {
   render() {
-    //dotenv.config('../../.env');
-    //console.log(process.env.REACT_APP_GOOGLE_KEY);
     return (
       <div className='map'>
         <Navbar />
@@ -44,14 +31,6 @@ class Map extends React.Component {
           containerElement={<div style={{ height: `400px` }} />}
           mapElement={<div style={{ height: `100%` }} />}
         />
-        {/* <div className='map-header' style={{ width: '100vw', hight: '100vh' }}>
-          <WrappedMap
-            googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY}`}
-            loadingElement={<div style={{ hight: `100%` }} />}
-            containerElement={<div style={{ hight: `100%` }} />}
-            mapElement={<div style={{ hight: `100%` }} />}
-          />
-        </div> */}
         <Filter />
       </div>
     );
