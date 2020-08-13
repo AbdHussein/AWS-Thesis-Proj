f8ebac92ce86ac9087671b083e6f238f7c7448e3
Nadera Qaoud <naderaqaoud2000@gmail.com>
2020-08-13 16:56:41 +0300

    details dashboard

--- client/src/components/dashboard/details/demoVideo.js
+++ client/src/components/dashboard/details/demoVideo.js
@@ -1,13 +1,23 @@
-import React from 'react';
-
+import React from "react";
+import ImageUpload from "../../imageUpload/imageUpload";
 class DemoVideo extends React.Component {
-    render() {
-        return (
-            <div className="demo-video">
-                Test From DemoVideo
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="demo-video">
+        <div className="upload-video">
+          {/* <h3>Add Your Demo Video</h3>
+          <ImageUpload ButtonText={"Add your demo"} /> */}
+        </div>
+
+        <div className="Thumbnail">
+          <div className="upload-Thumbnail">
+            <h3>Add Your Thumbnail</h3>
+            <ImageUpload ButtonText={"Add your Thumbnail"} />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default DemoVideo;
--- client/src/components/dashboard/details/description.js
+++ client/src/components/dashboard/details/description.js
@@ -1,10 +1,33 @@
 import React from 'react';
-
+import ImageUpload from '../../imageUpload/imageUpload';
+import PermMediaIcon from '@material-ui/icons/PermMedia';
 class Description extends React.Component {
+    state = {
+        text: "",
+       };      
+        
+        handelChange(e) {
+          this.setState({
+            [e.target.name]: e.target.value
+          });
+        }
     render() {
         return (
             <div className="description">
-                Test From Description
+                {/* <div className="add-gallery">
+                    <h3>Add Photes To Your Gallery</h3>
+                   <ImageUpload ButtonText = {"Add image"}/>
+                </div> */}
+                <form>
+                    <textarea className="post-area"
+                        name="text"
+                        cols="80"
+                        rows="30"
+                        value={this.state.text}
+                        onChange={this.handelChange.bind(this)}>
+                    </textarea>
+                    <button>Add Description</button>
+                </form>
             </div>
         )
     }
