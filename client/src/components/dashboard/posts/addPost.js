9ffae7e05d0e6ecd822f709cf272cb6610da6fe4
IbrahemAtef <bulbul.20001@gmail.com>
2020-08-16 19:17:41 +0300

    .

--- client/src/components/dashboard/posts/addPost.js
+++ client/src/components/dashboard/posts/addPost.js
@@ -4,72 +4,82 @@ import constants from '../../constants/Queries';
 const jwt = require('jsonwebtoken');
 
 class Add extends React.Component {
-   state = {
-    text: "",
-    imgUrl: null 
-   };      
-    
-    handelChange(e) {
-      this.setState({
-        [e.target.name]: e.target.value
-      });
-    }
+  state = {
+    text: '',
+    imgUrl: null,
+  };
 
-    updateImgUrl(url){
-        this.setState({
-            imgUrl: url
-        })
-    }
+  handelChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  }
 
-    async onSubmit(){
-        console.log(this.state.imgUrl);
-        try {
-            const data = jwt.verify(
-              localStorage.getItem('xTown'),
-              'somesuperdupersecret',
-              {
-                algorithm: 'HS256',
-              }
-            );
-            const addPost = await constants.addPost(
-              data.id,
-              this.state.imgUrl,
-              this.state.text
-            );
-            constants.request(addPost).then((result) => {
-              if(result.data.errors){
-                alert('Failed add Photo');
-              }
-            }).catch((err) => {
-              alert('Failed add Photo');
-            });
-        } catch (err) {
-            console.log(err);            
+  updateImgUrl(url) {
+    this.setState({
+      imgUrl: url,
+    });
+  }
+
+  async onSubmit() {
+    console.log(this.state.imgUrl);
+    try {
+      const data = jwt.verify(
+        localStorage.getItem('xTown'),
+        'somesuperdupersecret',
+        {
+          algorithm: 'HS256',
         }
+      );
+      const addPost = await constants.addPost(
+        data.id,
+        this.state.imgUrl,
+        this.state.text
+      );
+      constants
+        .request(addPost)
+        .then((result) => {
+          if (result.data.errors) {
+            alert('Failed add Photo');
+          }
+        })
+        .catch((err) => {
+          alert('Failed add Photo');
+        });
+    } catch (err) {
+      console.log(err);
     }
+  }
 
-    render() {
-        return (
-            <div className="dash-add">
-                <h2>Add your Post</h2>
-                <h4>What Is New?!</h4>
-                <form>
-                    <textarea  className="post-area"name="text" cols="80" rows="10" value={this.state.text} onChange={this.handelChange.bind(this)}></textarea>
-                    {/* <input type="file" name="image"  value={this.state.image} onChange={this.handelChange.bind(this)}/> */}
-                    <h4>Choose Post's Image</h4>
-                    <div className="upload">
-                        <ImageUpload getImgUrl= {this.updateImgUrl.bind(this)}/>
-                    </div>
-                    <button
-                    type='button'
-                    className='btn'
-                    onClick={this.onSubmit.bind(this)}
-                    >
-                    Add Post
-                    </button>
-                </form>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className='dash-add'>
+        <h2>Add your Post</h2>
+        <h4>What Is New?!</h4>
+        <form>
+          <textarea
+            className='post-area'
+            name='text'
+            cols='80'
+            rows='10'
+            value={this.state.text}
+            onChange={this.handelChange.bind(this)}
+          ></textarea>
+          {/* <input type="file" name="image"  value={this.state.image} onChange={this.handelChange.bind(this)}/> */}
+          <h4>Choose Post's Image</h4>
+          <div className='upload'>
+            <ImageUpload getImgUrl={this.updateImgUrl.bind(this)} />
+          </div>
+          <button
+            type='button'
+            className='btn'
+            onClick={this.onSubmit.bind(this)}
+          >
+            Add Post
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 export default Add;
