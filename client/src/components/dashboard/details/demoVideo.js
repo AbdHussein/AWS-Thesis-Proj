f55783152d86277af8f9c141f14212adf2eeaf03
AbdHussein <abdulrahmaanhussein97@gmail.com>
2020-08-17 11:46:31 +0300

    Video Upload implemented

--- client/src/components/dashboard/details/demoVideo.js
+++ client/src/components/dashboard/details/demoVideo.js
@@ -1,6 +1,39 @@
 import React from "react";
-import ImageUpload from "../../imageUpload/imageUpload";
+import VideoUpload from "../../videoUpload/videoUpload";
+import { makeStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import $ from 'jquery';
+
+
 class DemoVideo extends React.Component {
+  // classes = useStyles();
+  state ={
+    vidUrl: null
+  }
+  componentDidMount(){
+    $('.MuiCircularProgress-svg').hide();
+  }
+
+  uploadStarted(){
+    $('.MuiCircularProgress-svg').show();
+    $('.btn').hide();
+  }
+
+  getVidUrl(url){
+    this.setState({
+      vidUrl: url
+    }, () =>{
+      //TODO Enable button
+      $('.MuiCircularProgress-svg').hide();
+      $('.btn').show();
+    })
+  }
+
+  onSubmit(){
+    console.log('clicked');
+    console.log();
+  }
+  
   render() {
     return (
       <div className="demo-video">
@@ -12,8 +45,22 @@ class DemoVideo extends React.Component {
         <div className="Thumbnail">
           <div className="upload-Thumbnail">
             <h3>Add Your Thumbnail</h3>
-            <ImageUpload ButtonText={"Add your Thumbnail"} />
+            {/* <ImageUpload ButtonText={"Add your Thumbnail"} /> */}
+            <VideoUpload getVidUrl={this.getVidUrl.bind(this)} uploadStarted={this.uploadStarted.bind(this)}/>
           </div>
+          <br></br>
+          <div id="loadingDiv" >
+          <CircularProgress />
+          </div>
+          <br></br>
+          <button
+            type='button'
+            id = 'uploadVideoBtn'
+            className='btn'
+            onClick={this.onSubmit.bind(this)}
+          >
+            Add Video
+          </button>
         </div>
       </div>
     );
