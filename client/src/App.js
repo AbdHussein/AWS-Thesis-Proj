0b8ec8ecec69aa58cc30effca0a76cfdd0f45c2c
dolphen05 <34986941+dolphen05@users.noreply.github.com>
2020-08-17 11:13:58 +0300

    Front-End Feeds

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import ViewPost from './components/dashboard/posts/viewPost';
 import signIn from './components/Auth/SignIn';
 import signUp from './components/Auth/SignUp';
 import MyCart from './components/provider/myCart';
+import Feeds from './components/landing/feeds';
 
 function App() {
   return (
@@ -23,6 +24,7 @@ function App() {
           <Route path='/signIn' component={signIn} />
           <Route path='/signUp' component={signUp} />
           <Route path='/mycart' component={MyCart} />
+          <Route path='/feeds' component={Feeds} />
         </Switch>
       </div>
     </Router>
