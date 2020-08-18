69d08621d191e9897062f6d14fa81afc16bbbae1
dolphen05 <34986941+dolphen05@users.noreply.github.com>
2020-08-18 11:37:09 +0300

    whshList

--- client/src/components/provider/wishList.js
+++ client/src/components/provider/wishList.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Container } from '@material-ui/core';
 import Navbar from '../mainComp/navbar';
+import Footer from '../footer/footer';
 import Constants from '../constants/Queries'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 
 class WishList extends React.Component {
     state = {
@@ -26,10 +29,30 @@ class WishList extends React.Component {
 
     render() {
         return (
-            <div>
+            <div className="wish-list">
                 <Navbar provider={this.state.user} />
-                <div className="wish-list">
-                </div>
+                <div className="clear"></div>
+                <Container>
+                    <div className="wish-list-content">
+                        <div className="list-item">
+                            <div className="item-img">
+                                <img src={require(`../../images/29.jpg`)} alt="Item Picture" />
+                            </div>
+                            <div className="item-info">
+                                <h3>Item Heading</h3>
+                                <span>33$</span>
+                            </div>
+                            <div className="item-options">
+                                <FontAwesomeIcon icon={faTrashAlt} />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="wish-list-sidebar">
+                        Test From Side Bar
+                    </div>
+                </Container>
+                <div className="clear"></div>
+                <Footer />
             </div>
         )
     }
