d4834683b6d3c6ded4b4e03c7dba2b6eab836384
AbdHussein <abdulrahmaanhussein97@gmail.com>
2020-08-19 17:05:31 +0300

    changes

--- client/src/components/dashboard/dashboard.js
+++ client/src/components/dashboard/dashboard.js
@@ -24,6 +24,7 @@ class Dashboard extends React.Component {
     $('.dash-posts').click(function () {
       $('.dash-show-add').slideToggle();
     });
+    
     $('.dash-details').click(function () {
       $('.dash-des').slideToggle();
     });
@@ -38,7 +39,7 @@ class Dashboard extends React.Component {
       // const request = await Constants.request(query);
       // const provider = request.data.data.user;
       // this.setState({
-      //   provider,
+      //   provider,      
       // });
       await this.getProvider();
       await this.getPosts();
@@ -53,7 +54,7 @@ class Dashboard extends React.Component {
       const provider = request.data.data.user;
     this.setState({
       provider
-    }, async ()=>{
+    }, async () => {
       await this.getPosts();
     });
   }
